4ca576675d76f92f1a09f90d2bab5cd30adafa05
vantiger69 <vantiger817@gmail.com>
2024-11-07 21:27:06 +0300

    complete_lab

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ function NavBar() {
   );
 }
 
+
 function Home() {
   return (
     <div id="home">
@@ -16,15 +17,22 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About(){
+  return(
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  )
+}
+
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home/>
+      <About/>
     </div>
   );
 }
